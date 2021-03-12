644e17df327cedbdf8ac76f9e900db6e1380fa51
seansanchez951 <szsanchez@dons.usfca.edu>
2021-03-12 09:57:34 -0800

    First draft commit 3/12

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
+
 /**
  * An implementation of the ArrayList data structure
  * 
  * @author
  */
-public class MyArrayList<T>
-{
+public class MyArrayList<T> {
     protected T[] a;
     protected int size;
 
@@ -13,8 +14,10 @@ public class MyArrayList<T>
      */
     public MyArrayList() //makes new array list and sets size at 0
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        a = (T[]) new Object[10];
+        size=0;
     }
+
 
     /**
      * Adds an item to the end of the list
@@ -22,7 +25,10 @@ public class MyArrayList<T>
      */
     public void add(T item) //adds item to end of arraylist
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        if (size == a.length) {
+            grow_array();
+        }
+        a[size++] = item;
     }
 
     /**
@@ -30,18 +36,29 @@ public class MyArrayList<T>
      * @param pos the position to add the item at
      * @param item the item to add
      */
+
     public void add(int pos, T item) //adds item at given position
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        if (size == a.length) {
+            grow_array();
+        }
+        for (int i = size; i > pos; i--)
+            a[i] = a[i- 1];
+        a[pos] = item;
+        ++size;
+
     }
 
     /**
      * Gets the item at the position
      * @param pos position to get the item
      */
-    public T get(int pos) // returns item at index
+    public T get(int pos) throws Exception // returns item at index
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        if (pos < 0 || pos >= size) {
+            throw new Exception("Invalid Position");
+        }
+        return a[pos];
     }
 
     /**
@@ -49,43 +66,79 @@ public class MyArrayList<T>
      * @param pos the position to remove
      * @return the item at the position
      */
-    public T remove(int pos) //returns item after removing it from arraylist
+
+    public T remove(int pos) throws Exception //returns item after removing it from arraylist
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        if (pos < 0 || pos >= size) {
+            throw new Exception("Invalid Position");
+        }
+
+        // copy item in the array
+        T item = a[pos];
+
+        // move stuff in array over
+        for (int i = pos; i <size-1 ; i++) {
+            a[i] = a[i+1];
+        }
+
+        size -= 1;
+        return item;
     }
 
     /**
      * The size of the arraylist
-     * @return the size
+     * @return
      */
     public int size() //returns size
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return size;
     }
 
     /**
      * Rotates the array to the right by k steps.
      * @param k the number of steps to rotate right
      */
-    public void rotate(int k)
-    {
-        throw new UnsupportedOperationException("Not yet implemented!");
+
+    // setup private helper function that rotates reverses the array
+    public void reverse(T [] arr, int start, int end) {
+        while (start<end) {
+            T temp = (T) arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
+
+    public void rotate(int k) {
+        k = k % a.length;
+        // first reverse all numbers
+        reverse(a, 0, size-1);
+        reverse(a, 0 , k-1);
+        reverse(a, k, size-1);
+    }
+
+
     /**
      * Dynamically grow the array with the size of the ArrayList
      */
     private void grow_array() //doubles size of array
     {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        T [] new_arr = (T[]) new Object[size *= 2];
+        for (int i = 0; i < size; i++)
+            new_arr[i] = a[i];
+        a = new_arr;
     }
 
+    @Override
     public String toString()
     {
         String result = "";
-        for(int i=0; i< a.length; i++)
+        for(int i=0; i< size; i++)
         {
-            result = result + a + " ";
+            result = result + a[i] + " ";
         }
         return result;
     }
+
 }
--- Test.java
+++ Test.java
@@ -9,17 +9,17 @@ public class Test
      * Main function to run tests
      * @param args command arg line
      */
-    public static void main(String[] args) //change to test code
+    public static void main(String[] args) throws Exception  //change to test code
     {
         //test constructor
         MyArrayList<String> a1 = new MyArrayList<String>();
 
         //testing add
         a1.add("1");
-        a1.add("2");
-        a1.add("3");
-        a1.add("4");
-        a1.add("5");
+        a1.add(("2"));
+        a1.add(("3"));
+        a1.add(("4"));
+        a1.add(("5"));
         System.out.println(a1);
 
         //testing add2
@@ -28,9 +28,9 @@ public class Test
 
         //testing get
         System.out.println(a1.get(1));
-        
+
         //testing remove
-        System.out.println(a1.remove(0));
+        System.out.println(a1.remove(4));
         System.out.println(a1);
 
         //testing size
@@ -39,5 +39,6 @@ public class Test
         //testing rotate
         a1.rotate(2);
         System.out.println(a1);
+
     }
 }
